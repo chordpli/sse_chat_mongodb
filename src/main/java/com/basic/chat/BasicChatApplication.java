d0f6a1638ee817ee7bc0bc7af0a1b6155bd865ac
chordpli <chordplaylist@gmail.com>
2023-03-21 16:47:37 +0900

    build: build project
    1. mongodb 설정

---
+++ src/main/java/com/basic/chat/BasicChatApplication.java
@@ -0,0 +1,13 @@
+package com.basic.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicChatApplication.class, args);
+	}
+
+}
